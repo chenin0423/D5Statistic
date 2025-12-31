d7dc664b32af0d33c82b243707cafeac5bc123b5
bobkkblue <chienyec@gmail.com>
2025-12-31 10:26:16 -0800

    update

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ content = "<a>大區名單</a>"
 link = "https://chenin0423.synology.me:9090/names/"
 elem = '.div3 > .sub'
 updatePointContent(elem, ti, content, link)
-content = "<a>福音朋友登記表</a>"
+content = "<a>福音朋友名單登記表</a>"
 link = "https://forms.gle/SDs1mH7AhXw4TtZm7"
 elem = '.div4 > .sub'
 updatePointContent(elem, ti, content, link)
